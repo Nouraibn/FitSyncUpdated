af7b0c60f4549a97ef874dcd2b10164a3668fb78
rashaq3390 <rashaq3390@gmail.com>
2024-10-06 15:27:58 +0300

    rasha

---
+++ FitSync/RecommendationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationScreen.swift
+//  FitSync
+//
+//  Created by رشا القرني on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+struct RecommendationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationScreen()
+}
