c5bf4843105528a2dde0e5cc1d39d185e7db35d7
Nouraibn <Noura_alnassar@hotmail.com>
2024-10-06 14:46:33 +0300

    Initial Commit

---
+++ FitSync/FitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitSyncApp.swift
+//  FitSync
+//
+//  Created by Noura on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
